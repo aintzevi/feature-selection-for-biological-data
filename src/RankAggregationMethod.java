7e5e141742539e57cfe27bbab46c48cde4b050ac
aintzevi <aintzevi@csd.auth.gr>
2017-03-19 14:24:34 +0200

    - Implement MC3 method (third Markov Chain method) - Add some javadoc and comments - Add probabilitySum reset at MC2 method

--- src/RankAggregationMethod.java
+++ src/RankAggregationMethod.java
@@ -161,10 +161,13 @@ public class RankAggregationMethod {
      * The element must have a better ranking for at least half of the rankings to have a transition probability higher than zero
      */
     public void MC2() {
+        // Size of the transition matrix - also the number of elements/states
         int tableSize = SNPList.size();
+        // Counter to check if the majority of ranks of one state is better than the current state
         int counter = 0;
         // Create a 2D transition matrix, both dimension sizes equal to the number of SNPs
         double transitionMatrix[][] = new double[tableSize][tableSize];
+        // Variable to keep the sum of the probabilities of moving from the current state to another, to ultimately calculate the probability of staying to the current state
         double probabilitiesSum = 0.0;
 
         // Iterating through the elements to create the transition table
@@ -176,9 +179,10 @@ public class RankAggregationMethod {
                     // Count the number of rankings for which the first element has worse ranking than the second element
 
                     // If (for the corresponding ranking) the ranking of the first element is bigger than the second (aka is worse)
-                    if (SNPList.get(row).getSNPRank().get(i) > SNPList.get(column).getSNPRank().get(i))
+                    if (SNPList.get(row).getSNPRank().get(i) > SNPList.get(column).getSNPRank().get(i)) {
                         // Increment the counter
                         counter++;
+                    }
                 } // end of rankings for-loop
 
                 // If the rank of the first element is bigger than the second for more than half the rankings
@@ -193,7 +197,7 @@ public class RankAggregationMethod {
                 // Add probability of each state transition to get the sum
                 probabilitiesSum += transitionMatrix[row][column];
 
-                // Reset counter to zero
+                // Reset list majority counter to zero for next element iteration
                 counter = 0;
             } // end of columns for-loop
 
@@ -201,6 +205,50 @@ public class RankAggregationMethod {
             // to the value 1 - the sum of the probabilities to change to another state when in current state
             transitionMatrix[row][row] = 1 - probabilitiesSum;
 
+            // Reset probability counter for next current element iteration
+            probabilitiesSum = 0.0;
         } // end of rows for-loop
+    }
+
+    /**
+     * Markov Chain 3 method (Lin, 2010)
+     * Calculates the probability of moving from an element of lower ranking to one of a higher ranking (pairwise comparison)
+     * The probability of moving to another state is proportional to the number of lists that rank the new state higher than the current one.
+     */
+    public void MC3 () {
+        // Size of the transition matrix - also the number of elements/states
+        int tableSize = SNPList.size();
+        // Counter to keep the number of rankings where the current state has worse ranking than the new state it's compared to
+        int rankingsCounter = 0;
+        // Create a 2D transition matrix, both dimension sizes equal to the number of SNPs
+        double transitionMatrix[][] = new double[tableSize][tableSize];
+        // Variable to keep the sum of the probabilities of moving from the current state to another, to ultimately calculate the probability of staying to the current state
+        double probabilitiesSum = 0.0;
+
+        // Iterating through the elements to create the transition table
+        for (int row = 0 ; row < tableSize ; ++row) {
+            for (int column = 0; column < tableSize; ++column) {
+                // Iterating through the ranking lists of the current pair of SNPs (2 outer for-loops)
+                for (int i = 0 ; i < numberOfPrimaryRankings ; ++i) {
+                    // If (for the corresponding ranking) the ranking of the first element is bigger than the second (aka is worse)
+                    if (SNPList.get(row).getSNPRank().get(i) > SNPList.get(column).getSNPRank().get(i)) {
+                        // Increment the counter
+                        rankingsCounter  += 1;
+                    }
+                }
+                // Filling the transition matrix space according to the MC3 formula
+                transitionMatrix[row][column] = rankingsCounter/numberOfPrimaryRankings*tableSize;
+                // Adding calculated probability (from current to new state) to the probability sum (to help calculate the current -> current probability later)
+                probabilitiesSum += rankingsCounter/numberOfPrimaryRankings*tableSize;
+
+                // Reset rankings counter for next new element iteration
+                rankingsCounter = 0;
+            } // end of column for-loop (comparison of current element to all new elements)
+            // Calculate the probability from the current first element to itself. Value = 1 - the sum of the probabilities to the other elements
+            transitionMatrix[row][row] = 1 - probabilitiesSum;
+
+            // Reset probability counter for next current element iteration
+            probabilitiesSum = 0.0;
+        }
     }
 }
