3ac483dcd2d3103f9c1ac097d5902c03eec6f124
aintzevi <aintzevi@csd.auth.gr>
2017-06-20 14:37:10 +0300

    - Made subclass of abstract BordaMethod class - Tested

--- src/rankAggregationMethods/BordaMethods/BordaGeometricMean.java
+++ src/rankAggregationMethods/BordaMethods/BordaGeometricMean.java
@@ -1,28 +1,19 @@
 package rankAggregationMethods.BordaMethods;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Katerina Intzevidou on 15-May-17.
  * Email: <aintzevi@csd.auth.gr> <intz.katerina@gmail.com>
  */
-public class BordaGMean {
-/*
-    // List of the different rankings to aggregate
-    private List<Map<String, Double>> inputMap;
-
-    public BordaGMean(List<Map<String, Double>> inputMap) {
-        this.inputMap = inputMap;
-    }
-*/
+public class BordaGeometricMean extends BordaMethod{
 
     /**
      * Aggregates the numbers of the input list using the geometric mean formula as its aggregation function
      * @param numbersToBeAggregated List containing the numbers to be aggregated by this function
      * @return a double type value containing the result of the aggregation
      */
-    private Double computeAggregation (List<Double> numbersToBeAggregated) {
+    protected Double computeAggregation (List<Double> numbersToBeAggregated) {
         // Helper variable
         Double product = 1.0;
 
