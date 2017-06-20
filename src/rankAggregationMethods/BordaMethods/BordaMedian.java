15b5d374c0a05779c17404c61f5905b98406fe2d
aintzevi <aintzevi@csd.auth.gr>
2017-06-20 14:36:25 +0300

    - Made subclass of abstract BotdaMethod class - Tested

--- src/rankAggregationMethods/BordaMethods/BordaMedian.java
+++ src/rankAggregationMethods/BordaMethods/BordaMedian.java
@@ -7,25 +7,21 @@ import java.util.List;
  * Created by Katerina Intzevidou on 15-May-17.
  * Email: <aintzevi@csd.auth.gr> <intz.katerina@gmail.com>
  */
-public class BordaMedian {
-
-    // NOTE this list is NOT the one corresponding to the Map that comes as the output of the preprocessing stage.
-    // It is actually a transformed view that contains the scores of different rank systems for ONE SNP/Element
-    // TODO function that does this transformation -- See paper for details
+public class BordaMedian extends BordaMethod {
 
     /**
      * Aggregates the numbers of the input list using median as its aggregation function
      * @param numbersToBeAggregated input list containing the numbers to be aggregated
      * @return a double value containing the result of the aggregation
      */
-    private Double computeAggregation(List<Double> numbersToBeAggregated) {
+    protected Double computeAggregation(List<Double> numbersToBeAggregated) {
         // Store list size in temporary variable (readability reasons)
         int size = numbersToBeAggregated.size();
 
         // Sort list
         Collections.sort(numbersToBeAggregated);
 
-        // Check if odd or even
+        // Check if size is odd or even
         if (size % 2 == 0)     // If even
             // Return the mean value of the two "central" elements
             return (numbersToBeAggregated.get(size/2 - 1) + numbersToBeAggregated.get(size/2))/2;
